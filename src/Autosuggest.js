112af84f316ff980b9ccd9f130db1d4bbf66d553
Nicolas Hery <hery.nicolas@gmail.com>
2015-07-23 11:39:04 -0400

    Add onSuggestionsChange hook

--- src/Autosuggest.js
+++ src/Autosuggest.js
@@ -9,6 +9,7 @@ export default class Autosuggest extends Component { // eslint-disable-line no-s
     suggestionRenderer: PropTypes.func,     // Function that renders a given suggestion (must be implemented when suggestions are objects)
     suggestionValue: PropTypes.func,        // Function that maps suggestion object to input value (must be implemented when suggestions are objects)
     showWhen: PropTypes.func,               // Function that determines whether to show suggestions or not
+    onSuggestionsChange: PropTypes.func,    // This function is called when the suggestions displayed in the menu change
     onSuggestionSelected: PropTypes.func,   // This function is called when suggestion is selected via mouse click or Enter
     onSuggestionFocused: PropTypes.func,    // This function is called when suggestion is focused via mouse hover or Up/Down keys
     onSuggestionUnfocused: PropTypes.func,  // This function is called when suggestion is unfocused via mouse hover or Up/Down keys
@@ -20,6 +21,7 @@ export default class Autosuggest extends Component { // eslint-disable-line no-s
 
   static defaultProps = {
     showWhen: input => input.trim().length > 0,
+    onSuggestionsChange: () => {},
     onSuggestionSelected: () => {},
     onSuggestionFocused: () => {},
     onSuggestionUnfocused: () => {},
@@ -74,6 +76,7 @@ export default class Autosuggest extends Component { // eslint-disable-line no-s
   }
 
   setSuggestionsState(suggestions) {
+    this.props.onSuggestionsChange(suggestions);
     this.resetSectionIterator(suggestions);
     this.setState({
       suggestions: suggestions,
